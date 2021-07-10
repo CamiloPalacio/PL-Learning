2b95c70fb32700de30998b673fd6f8f7984cab22
CamiloPalacio <camilopalacio2k@gmail.com>
2021-07-09 19:16:49 -0500

    add converters

---
+++ JavaScript/AdvancedConverter/script.js
@@ -0,0 +1,43 @@
+(function(){
+    "use Strict";
+
+    let convertType = "miles";
+    const heading = document.querySelector('h1');
+    const intro = document.querySelector('p');
+    const answerDiv = document.getElementById('answer');
+    const form = document.getElementById('convert');
+
+    document.addEventListener('keydown', function(evt){
+        let key = evt.code;
+        
+        if(key === 'KeyK'){
+            convertType = 'kilometers';
+            heading.innerHTML = "Kilometers to Miles Converter";
+            intro.innerHTML = "Type in a number of kilometers and click the button to convert the distance to miles.";
+
+        }else if(key === 'KeyM'){
+            convertType = 'miles';
+            heading.innerHTML = "Miles to Kilometers Converter";
+            intro.innerHTML = "Type in a number of miles and click the button to convert the distance to kilometers.";
+        }
+    });
+
+    form.addEventListener('submit', function(evt){
+        evt.preventDefault();
+
+        const distance = parseFloat(document.getElementById('distance').value);
+
+        if(distance){
+            if(convertType === 'miles'){
+                const conversion = (distance * 1.609344).toFixed(3);
+                answerDiv.innerHTML = `<h2>${distance} miles converts to ${conversion} kilometers</h2>`;
+            }else{
+                const conversion = (distance * 0.621371192).toFixed(3);
+                answerDiv.innerHTML = `<h2>${distance} miles converts to ${conversion} kilometers</h2>`;
+            }
+            
+        }else{
+            answer.innerHTML = '<h2>Please provide a number!</h2>';
+        }
+    });
+})();
